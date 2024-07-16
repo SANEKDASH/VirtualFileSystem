6d976441a6c3afa9cbee6ba1589eb890b8b4fe39
sanekdash <dashinsky_alexander92@mail.ru>
2024-07-16 23:29:30 +0300

    situation hjuy v govne

--- src/dir.cpp
+++
@@ -1,7 +0,0 @@
-#include "dir.hpp"
-
-Dir::Dir(const std::string& name) : name(name), sz(0), n_files(0), files(nullptr) {}
-
-void Dir::change_name(const std::string& new_name) {
-    name = new_name;
-}
